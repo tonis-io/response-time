928174020126fc0a8270d155c45966aa1793cc01
Kyle Spraggs <theman@spiffyjr.me>
2015-06-18 22:46:36 -0500

    Few tweaks

--- src/Middleware.php
+++ src/Middleware.php
@@ -4,19 +4,43 @@ namespace Tonis\ResponseTime;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 
-class Middleware
+final class Middleware
 {
+    /** @var array */
+    private $config;
+
+    /**
+     * Middleware to set the X-Response-Time (default) header on the Response.
+     *
+     * - digits: set the number of digits (default: 3)
+     * - header: name of the header to set (default: X-Response-Time)
+     * - suffix: boolean to add suffix to time (default: true)
+     *
+     * @param array $config
+     */
+    public function __construct(array $config = [])
+    {
+        $defaults = [
+            'digits' => 3,
+            'header' => 'X-Response-Time',
+            'suffix' => true
+        ];
+        $this->config = array_merge($defaults, $config);
+    }
+
     public function __invoke(ServerRequestInterface $request, ResponseInterface $response, callable $next = null)
     {
         $server = $request->getServerParams();
 
         if (isset($server['REQUEST_TIME_FLOAT'])) {
-            $time = sprintf('%2.4fms', (microtime(true) - $server['REQUEST_TIME_FLOAT']) * 1000);
+            $ms   = (microtime(true) - $server['REQUEST_TIME_FLOAT']) * 1000;
+            $time = sprintf('%2.' . $this->config['digits'] . 'f%s', $ms, $this->config['suffix'] ? 'ms' : '');
         } else {
             $time = 'Unavailable';
         }
 
-        $response = $response->withHeader('X-Response-Time', $time);
-        return $next ? $next($request, $response) : $response;
+        $response = $next ? $next($request, $response) : $response;
+
+        return $response->withHeader($this->config['header'], $time);
     }
 }
